2d2882824c338a30e7ead52cf2d1f3a2919550b9
minagishl <mvxd568.co@icloud.com>
2024-07-26 23:19:56 +0900

    Create function to remove pictograms

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,16 @@ type Bindings = {
 
 const app = new Hono<{ Bindings: Bindings }>();
 
+// Function to delete emojis from text
+function emojiDeletion(text: string): string {
+  const regex = /[\uD800-\uDBFF][\uDC00-\uDFFF]/g;
+  return text.replace(regex, '');
+}
+
 app.get('/', (c) => {
   // Get & set parameter values
   let { name, background } = c.req.query();
+  name = emojiDeletion(name) || 'World';
   background = background || '#000';
 
   return c.text(`Hello, ${name}!`);
